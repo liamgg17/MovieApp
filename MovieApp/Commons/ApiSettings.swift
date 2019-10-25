c900d7be331059557536a1b1c72eb77dce80028d
Christian Martinez <christian@firewallsoluciones.com.mx>
2019-10-24 22:59:21 -0500

    Se añadió Settings de la API y el Entitiy Movie

---
+++ MovieApp/Commons/ApiSettings.swift
@@ -0,0 +1,18 @@
+//
+//  ApiSettings.swift
+//  MovieApp
+//
+//  Created by Christian Martinez on 10/24/19.
+//  Copyright © 2019 Christian Martinez. All rights reserved.
+//
+
+import Foundation
+
+struct ApiSettings {
+    
+      // MARK: Proiedades
+    
+    static let ApiBaseUrl: String = "https://api.themoviedb.org/3"
+    static let apiKey: String = "1d079f5ea0422f9924048bb364ca89ab"
+   
+}
---
+++ MovieApp/Modules/Movie/Entities/MovieEntity.swift
@@ -0,0 +1,50 @@
+//
+//  MovieEntity.swift
+//  MovieApp
+//
+//  Created by Christian Martinez on 10/24/19.
+//  Copyright © 2019 Christian Martinez. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class MovieEntity:  NSObject, Mappable {
+    
+    
+    
+    // MARK:  Propiedades
+    
+    var id: Int?
+    var title: String?
+    var overview: String?
+    var budget: Double?
+    var revenue: Double?
+    var releaseDate: Date?
+    var genres: [String]?
+    var posterPath: String?
+    
+    // MARK:  Métodos
+    
+    required init?(map: Map) {}
+    
+    // Mapeo del objeto Movie
+    
+    
+    func mapping(map: Map) {
+        
+        self.id <- map["id"]
+        self.title <- map["title"]
+        self.overview <- map["overview"]
+        self.budget <- map["budget"]
+        self.revenue <- map["revenue"]
+        self.releaseDate <- map["release_date"]
+//        if let genresOb = map.JSON["genres"] as? String: Any { self.genres = genresOb.compactMap({ $0["name"] as? String })
+//        }
+        self.posterPath <- map["poster_path"]
+        
+  
+    }
+    
+    
+}
